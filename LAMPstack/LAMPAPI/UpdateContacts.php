84d68a50a7af169b89f586097c35c33abaaf09f5
Dylanjamaludin <23643108+Dylanjamaludin@users.noreply.github.com>
2023-02-16 17:24:43 -0500

    Update UpdateContacts.php
    Added username, lastname, phone, and email input fields, so seperate queries can be made for each field in the contact that the user wants to change. Added 404 status code for unaffected contacts. Unaffected contacts means that the user attempted to update a contact that does not exist.

--- LAMPstack/LAMPAPI/UpdateContacts.php
+++ LAMPstack/LAMPAPI/UpdateContacts.php
@@ -14,99 +14,117 @@ if ($conn->connect_error)
 } 
 else
 {
+    $neededFieldNames = ["contactid","firstname","lastname", "email", "phone"];
 
-    if ($inData["field"] == "LastName")
+    foreach($neededFieldNames as $fieldName)
+    {
+        if(!isset($inData[$fieldName]))
+        {
+            http_response_code(400);
+            $data = [];
+            $data["error"] = "Body is missing $fieldName";
+            echo json_encode($data);
+            return;
+        }
+    }
+
+    if ($inData["lastname"] != "")
     {
         try
         {
             $stmt = $conn->prepare("UPDATE Contacts SET LastName  = ? WHERE ID = ?");
-            $stmt->bind_param("ss", $inData["string"], $inData["contactid"]);
-            $msg["error"] = "";
+            $stmt->bind_param("si", $inData["lastname"], $inData["contactid"]);
             $stmt->execute();
-            http_response_code(200);
-            sendResultInfoAsJson($msg);
         }
         catch(exception)
         {
             $msg["error"] = "unsuccessful";
             http_response_code(409);
             sendResultInfoAsJson($msg);
+            $stmt->close();
+            $conn->close();
         }
-
-        $stmt->close();
-        $conn->close();
     }
-    elseif ($inData["field"] == "FirstName")
+    if ($inData["firstname"] != "")
     {
         try
         {
             $stmt = $conn->prepare("UPDATE Contacts SET FirstName  = ? WHERE ID = ?");
-            $stmt->bind_param("ss", $inData["string"], $inData["contactid"]);
-            $msg["error"] = "";
+            $stmt->bind_param("ss", $inData["firstname"], $inData["contactid"]);
             $stmt->execute();
-            http_response_code(200);
-            sendResultInfoAsJson($msg);
+            
         }
         catch(exception)
         {
             $msg["error"] = "unsuccessful";
             http_response_code(409);
             sendResultInfoAsJson($msg);
+            $stmt->close();
+            $conn->close();
         }
 
-        $stmt->close();
-        $conn->close();
+        //$stmt->close();
+        //$conn->close();
     }
-    elseif ($inData["field"] == "Email")
+    if ($inData["email"] != "")
     {
         try
         {
             $stmt = $conn->prepare("UPDATE Contacts SET Email  = ? WHERE ID = ?");
-            $stmt->bind_param("ss", $inData["string"], $inData["contactid"]);
-            $msg["error"] = "";
+            $stmt->bind_param("ss", $inData["email"], $inData["contactid"]);
             $stmt->execute();
-            http_response_code(200);
-            sendResultInfoAsJson($msg);
         }
         catch(exception)
         {
             $msg["error"] = "unsuccessful";
             http_response_code(409);
             sendResultInfoAsJson($msg);
+            $stmt->close();
+            $conn->close();
         }
 
-        $stmt->close();
-        $conn->close();
+        //$stmt->close();
+        //$conn->close();
     }
-    elseif ($inData["field"] == "Phone")
+    if ($inData["phone"] != "")
     {
         try
         {
             $stmt = $conn->prepare("UPDATE Contacts SET Phone  = ? WHERE ID = ?");
-            $stmt->bind_param("ss", $inData["string"], $inData["contactid"]);
-            $msg["error"] = "";
+            $stmt->bind_param("ss", $inData["phone"], $inData["contactid"]);
             $stmt->execute();
-            http_response_code(200);
-            sendResultInfoAsJson($msg);
+            
         }
         catch(exception)
         {
             $msg["error"] = "unsuccessful";
             http_response_code(409);
             sendResultInfoAsJson($msg);
+            $stmt->close();
+            $conn->close();
         }
 
-        $stmt->close();
-        $conn->close();
+    }
+
+    $affected = $conn->affected_rows;
+
+    if($affected == 0)
+    {
+        http_response_code(404);
+        $msg = array();
+        $msg["error"] = "No records found";
+        echo json_encode($msg);
+        return;
     }
     else
     {
-        $msg["error"] = "unsuccessful";
-        http_response_code(409);
+        http_response_code(200);
+        $msg["error"] = "";
         sendResultInfoAsJson($msg);
-        $stmt->close();
-        $conn->close();
     }
+
+    $stmt->close();
+    $conn->close();
 }
 
 function getRequestInfo()
